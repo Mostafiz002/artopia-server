f9a925c8c0061746fb58cc84ee51fe6648a11f18
Mostafiz002 <mo.mahin4@gmail.com>
2025-11-10 00:57:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@web.8viqnbz.mongodb.net/?appName=web`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Artopia server is running");
+});
+
+app.listen(port, () => {
+  console.log("Artopia server is running on port", port);
+});
